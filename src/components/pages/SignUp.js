f893ebe2e8fa80c07d45ef0aafb3e774ae3afe52
Edgar Reynaga <edgar.reynaga@loop.colum.edu>
2019-02-11 19:51:35 -0600

    Disregard

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/signUp.css';
+
+class SignUp extends Component {
+    render() {
+        return (
+
+            <h1>Hello World!</h1>
+
+        );
+    }
+}
+
+export default SignUp;
