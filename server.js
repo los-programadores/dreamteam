e80242d0d8a9408adca238f46f289b988f0143ea
erithr <hvillalo@linkedin.biz>
2019-02-14 21:46:10 -0600

    test

--- server.js
+++ server.js
@@ -35,6 +35,15 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+//heroku stuff
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
