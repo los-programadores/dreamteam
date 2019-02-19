14a31187f9b4f64de6e9604570febec3ec0008a0
Ben deLong <bendelong45@gmail.com>
2019-02-18 21:04:13 -0600

    updated home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/HomeUser";
+import Home from "./components/Home";
 import LogIn from "./components/LogIn";
 import SignUpUser from "./components/SignUpUser";
 import SignUpGuide from "./components/SignUpGuide";
