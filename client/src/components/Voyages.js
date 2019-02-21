de1a281ae55694c637905e0dd23a94e3ccdbfa97
Ben deLong <bendelong45@gmail.com>
2019-02-20 19:24:10 -0600

    added google search functionality to voyages component

--- client/src/components/Voyages.js
+++ client/src/components/Voyages.js
@@ -1,23 +1,44 @@
 import React, { Component } from "react";
-import firebaseauth from "../firebase";
+import firebaseauth from "../firebase"
 import { Link } from "react-router-dom";
+/* global google */
 
-
-class Voyages extends Component {
-
-
-    createVoyage = () => {
-
+export default class Voyages extends Component {
+    state = {
+        chosenLocation: '',
     }
+    constructor(props) {
+        super(props);
+        this.autocompleteInput = React.createRef();
+        this.autocomplete = null;
+        this.handlePlaceChanged = this.handlePlaceChanged.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    componentDidMount() {
+        this.autocomplete = new google.maps.places.Autocomplete(this.autocompleteInput.current,
+            { "types": ["geocode"] });
+
+        this.autocomplete.addListener('place_changed', this.handlePlaceChanged);
+    }
+
+
+    handlePlaceChanged() {
+        const place = this.autocomplete.getPlace();
+        this.setState({ chosenLocation: place.formatted_address });
+    }
+
+
     render() {
         return (
             <div>
-                <h1>Welcome To Voyages Page</h1>
+                <h1>Welcom to the Voyages Page</h1>
+                <h2>build your Voyage Below:</h2>
+                <input ref={this.autocompleteInput} id="autocomplete" placeholder="Enter Location"
+                    type="text"></input>
+                <h2>{this.state.chosenAddress}</h2>
                 <button><Link to="/guides">Submit Voyage</Link></button>
             </div>
 
         )
     }
 }
-
-export default Voyages
