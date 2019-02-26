0db61c6ea99d3af0ef5b86b9c57d3b4a01d84ffe
Benjamin deLong <bendelong45@gmail.com>
2019-02-25 21:05:32 -0600

    Revert "display cards right"

--- client/src/components/GuideSelect.js
+++ client/src/components/GuideSelect.js
@@ -2,28 +2,15 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import guide from "./guideInfo/guideInfo";
+
 import CardComponent from "./Card/Card";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import "../styles/GuideSelect.css";
-let guideComponent;
+const guideComponent = guide.map(guideobject => <CardComponent {...guideobject} />)
 
 class Guide extends Component {
-
-    state = {
-        guides: null
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = { guides: guide };
-        this.loadCard()
-    }
-
-    loadCard = () => {
-        guideComponent = this.state.guides.map(guideobject => <CardComponent {...guideobject} />)
-    }
 
 
 
@@ -33,13 +20,12 @@ class Guide extends Component {
             <Row>
                 <Col>
                     <Col>
-                        {console.log(this.state.guides)}
                         <h1 className="text-center">Select your Guide</h1>
                         <hr></hr>
                     </Col>
                     <Col>
-                        <Row >
-                            <Col className="guide">
+                        <Row className="guide">
+                            <Col>
                                 {guideComponent}
                             </Col>
                         </Row>
